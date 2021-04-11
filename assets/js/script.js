7319e17957d0a46e48f1cfdb01ecf1a1b4ef8dd3
idnyaneshwaraxioned <dnyaneshwari@prdxn.com>
2021-04-11 19:55:12 +0530

    Toogle menu done

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,3 +143,19 @@ function stickybar() {
     });
 }
 stickybar()
+
+
+// ==========Hubburger menu ==========
+
+function toggle(){
+
+    let hamburger = document.querySelector('.hamburger');
+    let menu = document.querySelector('menu');
+
+    hamburger.addEventListener('click', function(){
+        hamburger.classList.toggle('active');
+        menu.classList.toggle('active');
+    });
+}
+
+toggle();
